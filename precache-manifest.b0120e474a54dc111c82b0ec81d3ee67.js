397262c9aa1710e4b8c4b77cc472def43e8b44ff
divyansh-kund <divyanshkundu8@gmail.com>
2020-07-07 13:16:34 +0530

    Made it deployble

---
+++ precache-manifest.b0120e474a54dc111c82b0ec81d3ee67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00202b7ead31d59dde5e5d892cd08a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bac1486d0d1c26dd116a",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0028b0c2e66a2699534d",
+    "url": "/static/css/main.047e9015.chunk.css"
+  },
+  {
+    "revision": "bac1486d0d1c26dd116a",
+    "url": "/static/js/2.38052948.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.38052948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0028b0c2e66a2699534d",
+    "url": "/static/js/main.44841ad4.chunk.js"
+  },
+  {
+    "revision": "aca0f5dabab3afe36e0f",
+    "url": "/static/js/runtime-main.e96107c2.js"
+  }
+]);
